52d8eac2f869ffe945f2bf495436352d40232221
Rodrigo <rodrigo.fonseca@orama.com.br>
2021-04-03 16:12:07 -0300

    Add project files.

---
+++ PocNugetSL/ClassTodo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PocNugetSL
+{
+    /// <summary>Classe de Todo Teste</summary>
+    public class ClassTodo
+    {
+        /// <summary>Processa um Todo</summary>
+        public void Todo() 
+        {
+        
+        }
+    }
+}
